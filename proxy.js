96054d9d215f43282e4dfb5d87e9d00888fba997
YuYu <126583565+YuYu-VN@users.noreply.github.com>
2024-01-13 23:02:36 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,20 +1,3 @@
 function FindProxyForURL(url, host) {
-    // If the hostname matches, send direct.
-    if (dnsDomainIs(host, "*.com") || dnsDomainIs(host, "*.net"))
-        return "DIRECT";
-
-    // If the protocol or URL matches, send direct.
-    if (url.substring(0, 4)=="ftp:")
-        return "DIRECT";
-
-    // If the requested website is hosted within the internal network, send direct.
-    if (isPlainHostName(host) || shExpMatch(host, "*.local") || isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") || isInNet(dnsResolve(host), "172.16.0.0", "255.240.0.0") || isInNet(dnsResolve(host), "192.168.0.0", "255.255.0.0") || isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0"))
-        return "DIRECT";
-
-    // If the IP address of the local machine is within a defined subnet, send to a specific proxy.
-    if (isInNet(myIpAddress(), "10.10.5.0", "255.255.255.0"))
-        return "PROXY 1.2.3.4:8080";
-
-    // DEFAULT RULE: All other traffic, use below proxies, in fail-over order.
     return "PROXY 192.168.128.2:10809";
 }
